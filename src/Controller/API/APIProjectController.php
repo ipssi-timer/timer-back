6ea21ddda2b15d49578954542f82bae9fc93debb
AURESANTA <auredcsanta@gmail.com>
2020-08-24 14:38:43 +0200

    ajout des features APIProject

--- src/Controller/API/APIProjectController.php
+++ src/Controller/API/APIProjectController.php
@@ -13,6 +13,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Swagger\Annotations as SWG;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -49,7 +53,31 @@ class APIProjectController extends AbstractController
     {
         $id = $request->query->get('id');
         $project = $this->em->getRepository(Project::class)->find($id);
-        $data = $this->serializer->serialize($project, 'json');
+
+        $encoders = [new JsonEncoder()]; // If no need for XmlEncoder
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $data = $serializer->serialize($project, 'json',[
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }]);
+
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/api/v1/project/group/list", name="project_group_list", methods={"POST"})
+     */
+    public function list()
+    {
+
+        $data = $this->serializer->serialize($this->getUser()->getGroups(), 'json',[
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }]);
 
         return new Response($data, 200, [
             'Content-Type' => 'application/json'
